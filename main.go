320315ff0b49fc522f4f61a6baaf62a40120b5fb
Marko <markolitovchenko24@gmail.com>
2023-02-19 15:04:09 -0500

    Implemented a graceful shutdown

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	//"fmt"
 	//"io/ioutil"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/markarko/Microservices/handlers"
 )
@@ -21,5 +24,26 @@ func main() {
 	sm.HandleFunc("/hello", hh.ServeHTTP)
 	sm.HandleFunc("/goodbye", gh.ServeHTTP)
 
-	http.ListenAndServe("localhost:9090", sm)
+	s := &http.Server{
+		Addr:         "localhost:9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
