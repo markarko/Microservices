59821269f60c0f6a22ee7217537311de659ef663
Marko <markolitovchenko24@gmail.com>
2023-02-20 19:36:13 -0500

    Added a Products type and a slice that contains a few products

---
+++ data/products.go
@@ -0,0 +1,35 @@
+package data
+
+import "time"
+
+type Product struct {
+	ID          int
+	Name        string
+	Description string
+	Price       float32
+	SKU         string
+	CreatedOn   string
+	UpdatedOn   string
+	DeletedOn   string
+}
+
+var productList = []*Product{
+	{
+		ID:          1,
+		Name:        "Latte",
+		Description: "Frothy milky coffee",
+		Price:       2.45,
+		SKU:         "abc323",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+	{
+		ID:          2,
+		Name:        "Espresso",
+		Description: "Short and strong coffee without milk",
+		Price:       1.99,
+		SKU:         "fjd34",
+		CreatedOn:   time.Now().UTC().String(),
+		UpdatedOn:   time.Now().UTC().String(),
+	},
+}
